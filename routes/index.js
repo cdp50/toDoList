5f80dd2927e818894e54a900e8c7edb52c099d8f
Claudio Parra <101138305+cdp50@users.noreply.github.com>
2023-06-08 07:57:53 -0600

    OAuth2.0 first try

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,28 @@
 const routes = require('express').Router();
-const controller = require('../controllers');
-const itemValidation = require('../utilities/validation_schema')
+const { auth } = require('express-openid-connect');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SESSION_SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.AUTH0_CLIENT_ID,
+  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+routes.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+routes.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+
 routes.use('/',require('./routes-swagger.js'));
-
-
-routes.get('/', controller.getAllItems);
-routes.get('/:id', controller.getItem);
-routes.post('/',itemValidation.checkSchema, controller.addItem);
-routes.put('/:id',itemValidation.checkSchema, controller.editItem);
-routes.delete('/:id', controller.deleteItem);
-
+routes.use('/items', require('./items.js'))
 
 
 module.exports = routes;
---
+++ routes/items.js
@@ -0,0 +1,47 @@
+const routes = require('express').Router();
+const controller = require('../controllers');
+const itemValidation = require('../utilities/validation_schema');
+const { requiresAuth } = require('express-openid-connect');
+
+// get all items
+routes.get('/',requiresAuth(), (req, res) => {
+    if (req.oidc.isAuthenticated()){
+        controller.getAllItems(req.res);
+    } else {
+        res.redirect('/login');
+    }
+    });
+// get item by id
+routes.get('/:id', requiresAuth(), (req, res) => {
+    if (req.oidc.isAuthenticated()){
+        controller.getItem(req, res);
+    } else {
+        res.redirect('/login');
+    }
+});
+// post new item
+routes.post('/', requiresAuth(), itemValidation.checkSchema, (req, res) => {
+    if(req.oidc.isAuthenticated()) {
+        controller.addItem(req, res);
+    } else {
+        res.redirect('/login');
+    }
+});
+// edit item
+routes.put('/:id',requiresAuth(), itemValidation.checkSchema, (req, res) => {
+    if(req.oidc.isAuthenticated()) {
+        controller.editItem(req, res);
+    } else {
+        res.redirect('/login');
+    }
+});
+// delete item
+routes.delete('/:id', requiresAuth(), (req, res) => {
+    if(req.oidc.isAuthenticated()) {
+        controller.deleteItem(req, res);
+    } else{
+        res.redirect('/login');
+    }
+});
+
+module.exports = routes;
