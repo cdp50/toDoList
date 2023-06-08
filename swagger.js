0327f82e93fd68b2a9876225186d11633a857477
Claudio Parra <101138305+cdp50@users.noreply.github.com>
2023-06-08 12:26:21 -0600

    improvement

--- swagger.js
+++ swagger.js
@@ -10,10 +10,10 @@ const doc = {
 };
 
 const outputFile = 'swagger.json';
-const endpointsFiles = ['routes/routes-swagger.js', 'routes/index.js', 'server.js'];
+const endpointsFiles = ['./server.js'];
 
 /* NOTE: if you use the express Router, you must pass in the 
    'endpointsFiles' only the root file where the route starts,
    such as index.js, app.js, routes.js, ... */
 
-swaggerAutogen(outputFile, endpointsFiles, doc);
+swaggerAutogen(outputFile, endpointsFiles, doc)
