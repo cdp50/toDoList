50fed945e4f5e153ab02186c02a80cceb909b4e8
Claudio Parra <101138305+cdp50@users.noreply.github.com>
2023-06-10 11:36:32 -0600

    id error fix 1

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,4 @@
-const { default: mongoose } = require('mongoose');
+const mongoose = require('mongoose');
 const model = require('../models/index');
 const createError = require('http-errors');
 
@@ -8,7 +8,11 @@ const controller = {}
 controller.getAllItems = async(req, res) => {
     try {
         const items = await model.find();
-        res.status(200).json(items);
+        if(!items){
+            throw createError(404, "Your to do list is empty");
+        }else{
+            res.status(200).json(items);
+        }
     } catch (err) {
         res.status(500).json({message: err.message})
     }
